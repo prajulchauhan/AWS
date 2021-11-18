1dfdde8b115a1cf53baa657a592c8b69e45153a9
Prajul Chauhan <prajul.chauhan2000@gmail.com>
2021-11-18 16:38:28 +0000

    script added

---
+++ Solutions/day_3/user_data_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y python3
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install -y ansible
+sudo apt install -y git
+cd /etc/ansible
+sed 'N a <[webservers]/n localhost>' /etc/ansible/hosts
+touch /home/ubuntu/.ssh/authorized_keys
+sudo chmod 0700 /home/ubuntu/.ssh
+sudo chmod 0645 /home/ubuntu/.ssh/authorized_keys
+ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa <<< y
+sudo cat /home/ubuntu/.ssh/id_rsa.pub >> /home/ubuntu/.ssh/authorized_keys
+cd /home/ubuntu
+git clone https://github.com/prajulchauhan/AWS.git
+cd /home/ubuntu/AWS
+git checkout -b develop
+git pull origin develop
+cd Solutions/day_3
+ansible-playbook webserver.yml
